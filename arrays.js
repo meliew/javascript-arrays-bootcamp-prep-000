8220dc5c4ed431405b4bc4748baf4d5335302f17
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-03 00:53:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,14 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+
+array.unshift(element)
+return array
+
+}
+  
+function destructivelyAddElementToBeginningOfArray(array, elememt) {
+  array[1](element)
+  return array
+    
+}
