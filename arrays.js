442e4b197fbf6dedea49ae4bd20d8d4714750428
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-03 00:55:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,9 +6,11 @@ array.unshift(element)
 return array
 
 }
+addElementToBeginningOfArray(chocolateBars, "baby ruth");
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array[1](element)
   return array
 
 }
+destructivelyAddElementToBeginningOfArray(chocolateBars, "hersheys")
